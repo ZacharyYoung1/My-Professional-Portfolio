958a5727d882380642a3c4af9dc56f7844454590
Zach Young <Zyoung.young9@gmail.com>
2021-10-18 18:09:59 -0400

    update

--- index.js
+++ index.js
@@ -13,15 +13,3 @@ $.ajax({
     }}
 });
 
-$.ajax({
-    url: "https://api.github.com/users/larafoster/repos",
-    method: "GET"
-}).then(results => {
-    console.log(results)
-    
-    for(let i = 0; i < results.length; i++){
-        if(results[i].stargazers_count != 0){
-        let article = "<article><header><h2>" + results[i].name + "</h2></header><p>" + results[i].description + "</p>" + "<ul><li><button><a href=" + results[i].html_url + " target= blank>Github Repo</a></button></li>"
-    work.append(article)
-    }}
-});
